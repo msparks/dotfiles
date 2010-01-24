7a2c7cf4c14f02bf42902bb66e65ef5c2b390465
Matt Sparks <ms@quadpoint.org>
2010-01-23 23:20:37 -0800

    Custom Mac OS X keybindings

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@ rsync -ruv .zsh* .vim* .gitconfig .screenrc ~
 mkdir -p ~/.vim/backup ~/.vim/temp
 mkdir -p ~/.ssh
 touch ~/.ssh/known_hosts
+
+if [[ `uname -s` == "Darwin" ]]; then
+  rsync -ruv Library ~
+fi
