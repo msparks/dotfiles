62d6a9f52733c9b743e5dac6fdbb32e7929ba578
Matt Sparks <ms@quadpoint.org>
2010-05-15 09:51:15 -0500

    Change install.sh script to Bash

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 rsync -ruv .zsh* .vim* .gitconfig .screenrc ~
 mkdir -p ~/.vim/backup ~/.vim/temp
 mkdir -p ~/.ssh
 touch ~/.ssh/known_hosts
 
-if [[ `uname -s` == "Darwin" ]]; then
+if [ `uname -s` == "Darwin" ]; then
   rsync -ruv Library ~
 fi
