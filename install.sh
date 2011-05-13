d2be334bbad59489b22dbb79b5603dcc49cfe26f
Matt Sparks <ms@quadpoint.org>
2011-05-12 17:28:40 -0700

    SSH ControlMaster sockets path

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-rsync -ruv .zsh* .vim* .gitconfig .screenrc .emacs* ~
+rsync -ruv .zsh* .vim* .gitconfig .screenrc .emacs* .ssh ~
+
+# vim stuff.
 mkdir -p ~/.vim/backup ~/.vim/temp
-mkdir -p ~/.ssh
+
+# ssh stuff.
+mkdir -m 700 -p ~/.ssh/sockets/
 touch ~/.ssh/known_hosts
 
 if [ `uname -s` == "Darwin" ]; then
