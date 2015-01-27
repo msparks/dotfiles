174d2fd87426e584440fc4b5d5a4e60495ae5899
Matt Sparks <ms@quadpoint.org>
2015-01-27 00:53:48 -0800

    emacs24: delete old lisp files on install.

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+cd $(dirname $0)
+
 rsync -av .zsh* .vim* .gitconfig .gitignore_global .screenrc .tmux.conf ~
 
 # Emacs configuration.
 rsync -av .emacs ~
 rsync -av .emacs.d ~
+rsync -av --delete --exclude="default.el" .emacs.d/lisp ~/.emacs.d
 rm -rf ~/.emacs-lisp ~/.emacs-mail
 
 mkdir -m 700 -p ~/.ssh
