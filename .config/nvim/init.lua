18a05ec9f95a2bdaadc496505c3867f53a0d15a1
Matt Sparks <ms@quadpoint.org>
2026-03-10 18:08:40 -0700

    nvim: add CUE-specific formatting.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -42,3 +42,33 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.cmd("startinsert")
   end
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "cue",
+  callback = function()
+    -- CUE (like Go) prefers hard tabs for indentation.
+    vim.opt_local.expandtab = false
+    -- Render tabs as 2 columns (personal preference).
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.softtabstop = 0
+  end,
+})
+
+vim.api.nvim_create_user_command("Format", function()
+  local ok, conform = pcall(require, "conform")
+  if not ok then
+    local ok_lazy, lazy = pcall(require, "lazy")
+    if ok_lazy then
+      lazy.load { plugins = { "conform.nvim" } }
+      ok, conform = pcall(require, "conform")
+    end
+  end
+
+  if not ok then
+    vim.notify("Format: conform.nvim not available", vim.log.levels.ERROR)
+    return
+  end
+
+  conform.format { lsp_fallback = true }
+end, {})
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -25,4 +25,9 @@ return {
       },
     },
   },
+
+  {
+    "jjo/vim-cue",
+    ft = { "cue" },
+  },
 }
